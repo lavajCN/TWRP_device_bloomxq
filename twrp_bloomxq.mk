915ac5d9599cceb67d8373d32732ec0bfa779832
lavaj <lava.j1@hotmail.com>
2026-01-11 19:45:28 +0800

    Update TWRP configuration inheritance

--- twrp_bloomxq.mk
+++ twrp_bloomxq.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from bloomxq device
 $(call inherit-product, device/samsung/bloomxq/device.mk)
